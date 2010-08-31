2cce1b87c0f5ab915f9ac45bc0422f5ed53f342c
jesjones@mindspring.com <jesjones@mindspring.com@9050f0cc-9cfd-11de-bca0-7bf21e48512a>
2010-08-31 03:21:59 +0000

    Rewrote ftester.py to not use make so that we can use it more easily on windows.

--- trunk/ftest/ftest.py
+++ trunk/ftest/ftest.py
@@ -1,33 +1,89 @@
 #!/usr/bin/python
+# Builds and runs the functional test exes (we don't use make files to do this so that we 
+# can run these tests on Windows without requiring make).
+import glob
 import os
+import platform
 import subprocess
 import sys
 
-def run(path, command):
-	process = subprocess.Popen(command, cwd = path, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+Verbose = False
+
+def runProcess(command, name):
+	if Verbose:
+		sys.stdout.write('   %s\n' % command)
+	process = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 	(outData, errData) = process.communicate()
 	if process.returncode != 0 or len(errData) > 0:
 		sys.stdout.flush()
 		sys.stderr.flush()
 		sys.stdout.write(outData)
 		sys.stderr.write(errData)
-		return False
+		raise ValueError('%s failed with error %s' % (name, process.returncode))
+
+def compileParser(command):
+	if platform.system() != 'Windows':
+		command = "mono --debug " + command
+	
+	runProcess(command, 'build parser')
+
+def compileExe(exeName, sources):
+	if platform.system() == 'Windows':
+		# TODO: should probably set flags to some default value if CSC_FLAGS is not set
+		command = "csc \\out:%s %s \\target:exe %s" % (exeName, os.getenv('CSC_FLAGS', ''), sources)
 	else:
-		return True
-		
-def doRun(dir):
+		command = "gmcs -out:%s %s -target:exe %s" % (exeName, os.getenv('CSC_FLAGS', ''), sources)
+	
+	runProcess(command, 'build exe')
+
+def buildParser(dir):
+	pattern = os.path.join(dir, 'Test*.peg')
+	pegs = glob.glob(pattern)
+	assert len(pegs) == 1
+	
+	parser = pegs[0].replace('.peg', '.cs')
+	if os.path.exists(parser):
+		os.remove(parser)
+	compileParser("../bin/peg-sharp.exe --out=%s %s" % (parser, pegs[0]))
+
+def buildExe(dir):
+	pattern = os.path.join(dir, 'Test*.peg')
+	pegs = glob.glob(pattern)
+	assert len(pegs) == 1
+	
+	app = os.path.join('../bin/ftest', os.path.basename(pegs[0].replace('.peg', '.exe')))
+	if os.path.exists(app):
+		os.remove(app)
+	sources = os.path.join(dir, '*.cs')
+	compileExe(app, ' '.join(glob.glob(sources)))
+	return app
+	
+def runExe(app):
+	command = app
+	if platform.system() != 'Windows':
+		command = "mono --debug " + command
+	
+	runProcess(command, 'run app')
+	
+def runTest(dir):
 	sys.stdout.write(dir + "...")
-	if run(dir, "make run"):
+	try:
+		if Verbose:
+			sys.stdout.write("\n")
+		buildParser(dir)
+		app = buildExe(dir)
+		runExe(app)
 		sys.stdout.write("passed\n")
+		return True
+	except ValueError, e:
+		sys.stdout.write("FAILED %s\n" % str(e))
+		return False
 
 failures = 0
 for root, dirs, files in os.walk("."):
 	for dir in dirs:
 		if dir[0].isdigit():
-			sys.stdout.write(dir + "...")
-			if run(dir, "make run"):
-				sys.stdout.write("passed\n")
-			else:
+			if not runTest(dir):
 				failures += 1
 
 exit(failures)
